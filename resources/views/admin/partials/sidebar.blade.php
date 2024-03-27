b9d6981ce07c50dda89a8c6df88fe310295e57d8
Rosyad Shidqi Dikpimmas <82706711+rosyadsd@users.noreply.github.com>
2024-03-27 16:00:37 +0700

    Dashboard Admin dan Landing page

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -4,14 +4,14 @@
 	        <div class="sidepanel-inner d-flex flex-column">
 		        <a href="#" id="sidepanel-close" class="sidepanel-close d-xl-none">&times;</a>
 		        <div class="app-branding">
-		            <a class="app-logo" href="home"><img class="logo-icon me-2" src="https://i.ibb.co/zhrvvPv/masak-mie-1.png" alt="logo"><span class="logo-text">Warmindo Aroma</span></a>
+		            <a class="app-logo" href="/home"><img class="logo-icon me-2" src="https://i.ibb.co/zhrvvPv/masak-mie-1.png" alt="logo"><span class="logo-text">Warmindo Aroma</span></a>
 	
 		        </div><!--//app-branding-->  
 			    <nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
 				    <ul class="app-menu list-unstyled accordion" id="menu-accordion">
 					    <li class="nav-item">
 					        <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-					        <a class="nav-link" href="home">
+					        <a class="nav-link" href="/home">
 						        <span class="nav-icon">
 						        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-house-door" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
 		  <path fill-rule="evenodd" d="M7.646 1.146a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5H9.5a.5.5 0 0 1-.5-.5v-4H7v4a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6zM2.5 7.707V14H6v-4a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v4h3.5V7.707L8 2.207l-5.5 5.5z"/>
@@ -23,7 +23,7 @@
 					    </li><!--//nav-item-->
 					    <li class="nav-item">
 					        <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-					        <a class="nav-link" href="menu">
+					        <a class="nav-link" href="/menu">
 						        <span class="nav-icon">
 						        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-folder" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path d="M9.828 4a3 3 0 0 1-2.12-.879l-.83-.828A1 1 0 0 0 6.173 2H2.5a1 1 0 0 0-1 .981L1.546 4h-1L.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3v1z"/>
@@ -35,7 +35,7 @@
 					    </li><!--//nav-item-->
 					    <li class="nav-item">
 					        <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-					        <a class="nav-link" href="order">
+					        <a class="nav-link" href="/order">
 						        <span class="nav-icon">
 						        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-list" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
@@ -143,21 +143,24 @@
 					    </ul><!--//footer-menu-->
 				    </nav>
 </div><!--//sidepanel-inner-->
+<!-- Script untuk menandai link aktif di sidebar -->
 <script>
-	document.addEventListener("DOMContentLoaded", function() {
-    const sidebar = document.getElementById('app-sidepanel');
-    const navLinks = document.querySelectorAll('.nav-link');
+    document.addEventListener("DOMContentLoaded", function() {
+        const sidebar = document.getElementById('app-nav-main');
+        const navLinks = sidebar.querySelectorAll('.nav-link');
 
-    navLinks.forEach(link => {
-        link.addEventListener('click', function() {
-            // Hapus semua kelas warna dari sidebar
-            sidebar.classList.remove('sidebar-color-home', 'sidebar-color-menu', 'sidebar-color-order');
+        // Mendapatkan path halaman saat ini
+        const currentPath = window.location.pathname;
 
-            // Dapatkan data-color link dan tambahkan ke kelas sidebar
-            const colorClass = this.dataset.color;
-            sidebar.classList.add(colorClass);
+        // Menandai link aktif berdasarkan path halaman
+        navLinks.forEach(link => {
+            // Menghapus kelas active dari setiap link
+            link.classList.remove('active');
+            
+            // Memeriksa apakah path halaman saat ini cocok dengan path link
+            if (currentPath === link.getAttribute('href')) {
+                link.classList.add('active');
+            }
         });
     });
-});
-
 </script>
